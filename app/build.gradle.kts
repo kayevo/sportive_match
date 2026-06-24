787890f38c5bfabc6ed24a30cb6cfb228b5eaf8e
Johantan Kayevo <kayevo@protonmail.com>
2026-06-24 14:19:28 -0300

    settings: add Navigation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
 }
