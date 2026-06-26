43ff51cbe87f828aba192c27bd8b8e0f40155bab
Johantan Kayevo <kayevo@protonmail.com>
2026-06-26 14:51:03 -0300

    feature: send and process candidates.

--- app/src/main/java/com/kayevo/sportive_match/platform/ui/generate_team_match/GenerateTeamMatchScreen.kt
+++ app/src/main/java/com/kayevo/sportive_match/platform/ui/generate_team_match/GenerateTeamMatchScreen.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -19,13 +21,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.kayevo.sportive_match.platform.ui.theme.Sportive_matchTheme
+import androidx.compose.foundation.lazy.items
 
 @SuppressLint("ViewModelConstructorInComposable")
 @Preview(showBackground = true)
@@ -46,13 +50,16 @@ fun GenerateTeamMatchScreenPreview() {
 @Composable
 fun GenerateTeamMatchScreen(
     navController: NavController,
-    viewModel: ViewModel,
+    viewModel: GenerateTeamMatchVM,
     modifier: Modifier = Modifier
 ) {
-    val candidates = "Name A, Name B"
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
     val team1 = listOf("Name 1", "Name 2")
     val team2 = listOf("Name 3")
     val nextToPlay = listOf("Name 4")
+    val blueBallEmoji = "\uD83D\uDD35"
+    val greenBallEmoji = "\uD83D\uDFE2"
+    val nextEmoji = "⏭"
 
     Column(
         modifier = modifier
@@ -71,9 +78,11 @@ fun GenerateTeamMatchScreen(
         Spacer(modifier = Modifier.height(24.dp))
 
         OutlinedTextField(
-            value = candidates,
-            onValueChange = {},
-            label = { Text("Candidates") },
+            value = uiState.value.sentCandidates,
+            onValueChange = { candidates ->
+                viewModel.processIntent(GenerateTeamMatchIntent.ChangeCandidates(candidates))
+            },
+            label = { Text("Sent candidates") },
             placeholder = {
                 Text("John, Mary, Peter, Alice...")
             },
@@ -83,19 +92,41 @@ fun GenerateTeamMatchScreen(
 
         Spacer(modifier = Modifier.height(24.dp))
 
+        Card(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = "Candidates",
+                    style = MaterialTheme.typography.titleMedium
+                )
+
+                Spacer(modifier = Modifier.fillMaxWidth().height(8.dp))
+
+                LazyColumn(Modifier.fillMaxWidth().height(100.dp)) {
+                    items(uiState.value.candidates) { candidates ->
+                        Text("• $candidates")
+                    }
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
 
             TeamCard(
-                title = "🔵 Team 1",
+                title = "$blueBallEmoji Team 1",
                 players = team1,
                 modifier = Modifier.weight(1f)
             )
-
             TeamCard(
-                title = "🔴 Team 2",
+                title = "$greenBallEmoji Team 2",
                 players = team2,
                 modifier = Modifier.weight(1f)
             )
@@ -110,7 +141,7 @@ fun GenerateTeamMatchScreen(
                 modifier = Modifier.padding(16.dp)
             ) {
                 Text(
-                    text = "⏭ Next to Play",
+                    text = "$nextEmoji Next to Play",
                     style = MaterialTheme.typography.titleMedium
                 )
 
@@ -128,7 +159,11 @@ fun GenerateTeamMatchScreen(
             onClick = {
                 // TODO Generate teams
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Black,
+                contentColor = Color.White
+            )
         ) {
             Text("Generate Teams")
         }
--- app/src/main/java/com/kayevo/sportive_match/platform/ui/generate_team_match/GenerateTeamMatchVM.kt
+++ app/src/main/java/com/kayevo/sportive_match/platform/ui/generate_team_match/GenerateTeamMatchVM.kt
@@ -2,6 +2,59 @@ package com.kayevo.sportive_match.platform.ui.generate_team_match
 
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
-class GenerateTeamMatchVM: ViewModel(){
+class GenerateTeamMatchVM : ViewModel() {
+    private val _uiState = MutableStateFlow(
+        GenerateTeamMatchUiState(
+            "",
+            emptyList(),
+            emptyList(),
+            emptyList(),
+            emptyList(),
+        )
+    )
+    val uiState: StateFlow<GenerateTeamMatchUiState> = _uiState
+
+    fun processIntent(intent: GenerateTeamMatchIntent) {
+        when (intent) {
+            is GenerateTeamMatchIntent.ChangeCandidates -> {
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        sentCandidates = intent.sentCandidates,
+                        candidates = splitCandidates(intent.sentCandidates)
+                    )
+                }
+            }
+
+            GenerateTeamMatchIntent.GenerateTeams -> {
+                generateTeams()
+            }
+
+        }
+    }
+
+    private fun splitCandidates(candidates: String): List<String> = candidates
+        .split(",")
+        .map { it.trim() }
+        .filter { it.isNotEmpty() }
+
+    private fun generateTeams() {
+        // TODO generate teams.
+    }
+}
+
+data class GenerateTeamMatchUiState(
+    val sentCandidates: String,
+    val candidates: List<String>,
+    val team1: List<String>,
+    val team2: List<String>,
+    val nextCandidates: List<String>,
+)
+
+sealed interface GenerateTeamMatchIntent {
+    data class ChangeCandidates(val sentCandidates: String) : GenerateTeamMatchIntent
+    data object GenerateTeams : GenerateTeamMatchIntent
 }
